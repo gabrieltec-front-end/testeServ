7c3fd10ee6461d5386b9dc4e0b83aae2539e8a57
grabieltec-front-end <supremacia692@gmail.com>
2026-08-23 19:21:05 -0300

    Criação de rota secure

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,64 @@
-
-import './App.css'
+import "./App.css";
 import {
-  BrowserRouter as Router, Route, Routes,
-} from 'react-router-dom'
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  Outlet
+} from "react-router-dom";
 
-
+import Cookies from "js-cookie";
 //PAGE
-import { Cadastro, Home, Login, ListUsuarios, CadastroP, ListP, CadastroI } from "@/page"
+import {
+  Cadastro,
+  Home,
+  Login,
+  ListUsuarios,
+  CadastroP,
+  ListP,
+  CadastroI,
+} from "@/page";
 function App() {
-
+  const SecureRoute = () => {
+    const cookiesAuth = Cookies.get("authorization");
+    if (!cookiesAuth) {
+      alert("Login necessario");
+      return <Navigate to={"/"} replace />;
+    }
+    return <Outlet />
+  };
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<Login />} >Login</Route>
-          <Route path="/cadastroI" element={<CadastroI />}>CadastroI</Route>
-          <Route path='/home' element={<Home />} >Home</Route>
-          <Route path='/cadastro' element={<Cadastro />} >Cadastro</Route>
-          <Route path='/listaUsuarios' element={<ListUsuarios />} >Cadastro</Route>
-          <Route path='/cadastroP' element={<CadastroP />} >Cadastro</Route>
-          <Route path='/listaProdutos' element={<ListP />} >Cadastro</Route>
+          <Route path="/" element={<Login />}>
+            Login
+          </Route>
+          <Route path="/cadastroI" element={<CadastroI />}>
+            CadastroI
+          </Route>
+          <Route path="/home" element={<Home />}>
+            Home
+          </Route>
 
+          <Route element={<SecureRoute />}>
+
+            <Route path="/cadastro" element={<Cadastro />}>
+              Cadastro
+            </Route>
+            <Route path="/listaUsuarios" element={<ListUsuarios />}>
+              Cadastro
+            </Route>
+            <Route path="/cadastroP" element={<CadastroP />}>
+              Cadastro
+            </Route>
+            <Route path="/listaProdutos" element={<ListP />}>
+              Cadastro
+            </Route>
+          </Route>
         </Routes>
-
       </Router>
-
     </>
-  )
+  );
 }
-export default App
-
+export default App;
